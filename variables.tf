5d1e5b1ab6c0c168545751d2a3e61cbef7da784e
Christian Baumgartner <christian.baumgartner@glueckkanja.com>
2025-10-17 10:24:38 +0200

    Add telemetry configuration and enable telemetry variable

--- variables.tf
+++ variables.tf
@@ -72,3 +72,14 @@ variable "repository_permissions" {
   default     = []
   nullable    = false
 }
+
+variable "enable_telemetry" {
+  type        = bool
+  default     = true
+  description = <<DESCRIPTION
+This variable controls whether or not telemetry is enabled for the module.
+For more information see <https://aka.ms/avm/telemetryinfo>.
+If it is set to false, then no telemetry will be collected.
+DESCRIPTION
+  nullable    = false
+}
